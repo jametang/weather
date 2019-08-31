2091ed6b47a6b1fd7fa2b4db896648d5db0979bf
tangli <18782994956@163.com>
2019-08-31 17:48:24 +0800

    修改包名为com.southwest.weather

---
+++ app/src/main/java/com/southwest/weather/dataInterface/DataInterface.java
@@ -0,0 +1,7 @@
+package com.southwest.weather.dataInterface;
+
+public interface DataInterface {
+    void setCid(String cid);
+    void deleteID(int index);
+    void changeBack(String condCode);
+}
